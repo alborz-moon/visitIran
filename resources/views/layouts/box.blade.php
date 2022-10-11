4239c55a6d36146e43ef21bceb8d3dc4f3074428
Alborz <alborzhacker@gmail.com>
2022-10-11 12:51:51 +0330

    Shimmer

--- resources/views/layouts/box.blade.php
+++ resources/views/layouts/box.blade.php
@@ -17,38 +17,22 @@
                                 <a href="#" class="cursorPointer">
                                     <div class="swiper-slide customWidthBox">
                                     <!-- start of product-card -->
-                                    <div class="product-card customBorderBoxShadow">
-                                        <div class="product-thumbnail">
-                                            <a href="#" target="_blank">
-                                                <img class="boxLoadingEmptyContent backgroundColorNone" src="{{ asset('theme-assets/images/logo.png') }}" alt="product title">
-                                            </a>
+                                    <div class="product-card customBorderBoxShadow SimmerParent">
+                                        <div class="SimmerParent">
+                                        <div class="shimmerBG media">
                                         </div>
-                                        <div class="product-card-body">
-                                            <h2 class="product-title boxLoadingEmptyContent boxDescription " >
-
-                                            </h2>
-                                            <div class="spaceBetween mt-3 mb-3 boxLoadingEmptyContent">
-                                                <span class="fontSize11 colorRed whiteSpaceNoWrap visibilityHidden">موجودی تنها دو عدد</span>
-                                                <span class="visibilityHidden"><i class="icon-visit-staroutline visibilityHidden"></i><i class="icon-visit-staroutline"></i><i class="icon-visit-staroutline"></i><i class="icon-visit-star"></i><i class="icon-visit-star"></i></span>
-                                            </div>
-                                            <div class="product-price fa-num boxLoadingEmptyContent m-neg-10">
-                                                <div class="visibilityHidden">
-                                                    <span class="fontSize15 pl-10 position-relativevisibilityHidden"><img src="{{ asset('theme-assets/images/svg/off.svg') }}" alt=""><span class="position-absolute fontSize10 colorWhite r-0 customOff visibilityHidden">20%</span></span>
-                                                    <del class="customlineText textColor fontSize15 visibilityHidden">26,900,000</del>
+                                          <div class="p-32">
+                                           <div class="shimmerBG title-line"></div>
+                                                  <div class="shimmerBG title-line end"></div>
+                                                  <div class="shimmerBG content-line m-t-24"></div>
+                                                  <div class="shimmerBG content-line end"></div>
                                                 </div>
-                                                <div class="fontSize20 visibilityHidden"><span
-                                                        class="fontSize20 colorYellow visibilityHidden">ت</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="product-card-footer mb-2 boxLoadingEmptyContent mt-2">
-                                            <div class="textColor visibilityHidden"><span class="bold">از</span> تهران دستی</div>
-                                        </div>
+                                              </div>
                                     </div>
                                     <!-- end of product-card -->
                                     </div>
                                 </a>
-                                                                <a href="#" class="cursorPointer">
+                                <a href="#" class="cursorPointer">
                                     <div class="swiper-slide customWidthBox">
                                     <!-- start of product-card -->
                                     <div class="product-card customBorderBoxShadow">
