53ec02c5c42584b285d0e3f3f6505fc3b21ea5d1
Alborz <alborzhacker@gmail.com>
2022-10-27 13:35:19 +0330

    favorites

--- resources/views/shop/profile/profile-favorites.blade.php
+++ resources/views/shop/profile/profile-favorites.blade.php
@@ -11,33 +11,6 @@
                             <div class="ui-box-content">
                                 <div class="product-list">
                                     <div id="favorites" class="row">
-                                        <div class="col-md-6 mb-3">
-                                            <!-- start of product-list-item -->
-                                            <div class="product-list-item border-bottom pb-3">
-                                                <div class="thumbnail">
-                                                    <a href="#"><img src="./theme-assets/images/carts/01.jpg"
-                                                            alt=""></a>
-                                                </div>
-                                                <div class="detail">
-                                                    <a href="#" class="title fs-7 fw-bold mb-2">هدفون بی سیم سامسونگ مدل
-                                                        Galaxy
-                                                        Buds
-                                                        Live
-                                                    </a>
-                                                    <div class="price fa-num">
-                                                        <span class="fw-bold">2,110,000</span>
-                                                        <span class="currency">تومان</span>
-                                                    </div>
-                                                    <div class="action">
-                                                        <a href="#" class="btn btn-circle btn-outline-light"
-                                                            data-remodal-target="remove-from-favorite-modal"><i
-                                                                class="ri-close-line"></i></a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <!-- end of product-list-item -->
-                                        </div>
-                                        <!-- end -->
                                     </div>
                                 </div>
                             </div>
@@ -46,6 +19,23 @@
                 </div>
             </div>
         </main>
+            <!-- start of remove-from-favorite-modal -->
+            <div class="remodal remodal-xs" data-remodal-id="remove-from-favorite-modal"
+                data-remodal-options="hashTracking: false">
+                <div class="remodal-header">
+                    <button data-remodal-action="close" class="remodal-close"></button>
+                </div>
+                <div class="remodal-content">
+                    <div class="text-muted fs-7 fw-bold mb-3">
+                        آیا مطمئنید که این محصول از لیست مورد علاقه شما حذف شود؟
+                    </div>
+                </div>
+                <div class="remodal-footer">
+                    <button data-remodal-action="cancel" class="btn btn-sm btn-outline-light px-3 me-2">خیر</button>
+                    <button class="btn btn-sm btn-primary px-3">بله</button>
+                </div>
+            </div>
+            <!-- end of remove-from-favorite-modal -->
 @stop
 
 @section('footer')
@@ -71,13 +61,26 @@
                         html +='<div class="col-md-6 mb-3">';
                         html +='<div class="product-list-item border-bottom pb-3">';
                         html +='<div class="thumbnail">';
-                        html +='<a href="#"><img src="./theme-assets/images/carts/01.jpg" alt=""></a>';
+                        html +='<a href="' + res.data[i].href + '"><img src="' + res.data[i].img + '" alt="' + res.data[i].alt + '"></a>';
                         html +='</div>';
                         html +='<div class="detail">';
-                        html +='<a href="#" class="title fs-7 fw-bold mb-2">هدفون بGalaxy</a>';
+                        html +='<a href="' + res.data[i].href + '" class="title fs-7 fw-bold mb-2">' + res.data[i].name + '</a>';
+                        html +='<div class="d-flex justify-content-start align-items-center"';
+                        if(res.data[i].rate != null) {
+                            for(var j = 5; j >= 1; j--) {
+                                if(j <= res.data[i].rate)
+                                    html += '<i class="icon-visit-star me-1 fontSize21"></i>';
+                                else
+                                    html += '<i class="icon-visit-staroutline me-1 fontSize14"></i>';
+                            }
+                        }
+                        html +='</div>';
                         html +='<div class="price fa-num">';
-                        html +='<span class="fw-bold">2,110,000</span>';
-                        html +='<span class="currency">تومان</span>';
+                        if (res.data[i].off != null){
+                            html +='<div class="fw-bold fontSize15 colorRed">' + res.data[i].off + '</div>';
+                        }
+                        html +='<span class="fw-bold fontSize15">' + res.data[i].price + '</span>';
+                        html +='<span class="currency colorYellow">ت</span>';
                         html +='</div>';
                         html +='<div class="action">';
                         html +='<a href="#" class="btn btn-circle btn-outline-light" data-remodal-target="remove-from-favorite-modal">';
@@ -88,6 +91,7 @@
                         html +='</div>';
                         html +='</div>';
                     }
+                    $("#favorites").empty().append(html);
                 }
             }
         });
