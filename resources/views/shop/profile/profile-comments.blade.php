608fc9dd9f618fd7a2e1a11e0074fed70a06af9c
Alborz <alborzhacker@gmail.com>
2022-10-27 12:53:24 +0330

    MyComments-Completed

--- resources/views/shop/profile/profile-comments.blade.php
+++ resources/views/shop/profile/profile-comments.blade.php
@@ -40,19 +40,34 @@
                 for(var i = 0; i < res.data.length; i++) {
                     html +='<div class="col-lg-6 mb-4">';
                     html +='<div class="border rounded p-3">';
-                    html +='<a href="#" class="d-flex align-items-center link pb-3">';
+                    html +='<a href="' + res.data[i].href + '" class="d-flex align-items-center link pb-3">';
                     html +='<img src="' + res.data[i].img + '" width="60" alt="">';
-                    html +='<span class="fs-7 fw-bold"> و رم 4 گیگابایت</span>';
+                    html +='<span class="fs-7 fw-bold">' + res.data[i].product + '</span>';
                     html +='</a>';
                     html +='<div class="comment">';
-                    html +='<div class="comment-header"><span>۱۵ تیر ۱۴۰۰</span></div>';
-                    html +='<div class="comment-body"><p>در کل</p>';
+                    html +='<div class="comment-header"><span> در ' + res.data[i].created_at + '</span></div>';
+                    html +='<div class="comment-body"><p>' + res.data[i].msg + '</p>';
+                    html += '<div class="d-flex align-items-center justify-content-center">';
+                    if(res.data[i].rate != null) {
+                        for(var j = 5; j >= 1; j--) {
+                            if(j <= res.data[i].rate){
+                                html += '<i class="icon-visit-star me-1 fontSize21"></i>';
+                            }else{
+                                html += '<i class="icon-visit-staroutline me-1 fontSize14"></i>';
+                            }
+                        }
+                    }
+                    html += '</div>';
                     html +='<ul>';
-                    html +='<li class="comment-evaluation positive">دوربین قوی</li>';
-                    html +='<li class="comment-evaluation negative">حسگر اثر انگشت کمی ضعیفه</li>';
+                    for(var j = 0; j < res.data[i].positive.length; j++) {
+                        html += '<li class="comment-evaluation positive">' + res.data[i].positive[j] + '</li>';
+                    }
+                    for(var j = 0; j < res.data[i].negative.length; j++) {
+                        html += '<li class="comment-evaluation negative">' + res.data[i].negative[j] + '</li>';
+                    }  
                     html +='</ul>';
                     html +='</div>';
-                    html +='<div class="d-flex align-items-center justify-content-flex-end">';
+                    html +='<div class="d-flex align-items-center justify-content-end">';
                     html +='<a href="#" class="btn btn-link text-danger">حذف <i class="ri-delete-bin-5-line ms-1"></i></a>';
                     html +='</div>';
                     html +='</div>';
