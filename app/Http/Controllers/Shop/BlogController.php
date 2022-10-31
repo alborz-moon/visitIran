fc25cb744270b1d84c46727f576f6c3428efcb86
mohammad ghane <mghaneh1375@yahoo.com>
2022-10-31 14:11:53 +0330

    blog list completed.

--- app/Http/Controllers/Shop/BlogController.php
+++ app/Http/Controllers/Shop/BlogController.php
@@ -194,6 +194,8 @@ class BlogController extends BlogHelper
         )
             return Redirect::route('403');
 
+        dd(BlogResource::make($blog)->toArray($request));
+
         return view('shop.blog', [
             'blog' => BlogResource::make($blog)->toArray($request)
         ]);
--- resources/views/shop/blog-list.blade.php
+++ resources/views/shop/blog-list.blade.php
@@ -6,13 +6,13 @@
                 <!-- start of box => contact-us -->
                 <h3 class="mt-2 mb-5">اتاق خبر</h3>
                 <div class="d-flex justifuy-content-spacebeween align-item-center mb-5">
-                    <select id="setDistinctTags" class="jet-select__control w-100 py-2 px-5" name="category">
+                    <select id="distinctTags" class="jet-select__control w-100 py-2 px-5" name="category">
 			        </select>
                     <select id="orderBy" class="jet-sorting-select w-100 py-2 px-5" name="select-name">
 						<option value="-1">مرتب سازی</option>
-						<option {{ isset($orderBy) && $orderBy == 'header_asc' ? 'selected' : '' }} value="header_asc">عنوان</option>
-						<option {{ isset($orderBy) && $orderBy == 'createdAt_desc' ? 'selected' : '' }} value="createdAt_desc">جدیدترین‌ها</option>
-						<option {{ isset($orderBy) && $orderBy == 'seen_desc' ? 'selected' : '' }} value="seen_desc">پربازدیدترین‌ها</option>
+						<option value="header_asc">عنوان</option>
+						<option value="createdAt_desc">جدیدترین‌ها</option>
+						<option value="seen_desc">پربازدیدترین‌ها</option>
 					</select>
                		<input class="jet-search-filter__input w-100 py-2 px-5 d-none d-lg-block" type="search" autocomplete="off" value="" placeholder="جست‌وجو بر اساس کلمات کلیدی">
                 </div>
@@ -37,12 +37,14 @@
         
         $(document).ready(function() {
 
-            // filter();
-            $("#setDistinctTags").on('change', function() {
-               filter() ;
+            filter();
+
+            $("#distinctTags").on('change', function() {
+               filter();
             });
+
             $("#orderBy").on('change', function() {
-               filter() ;
+               filter();
             });
 
             function buildQuery() {
@@ -50,38 +52,31 @@
                 let query = new URLSearchParams();
                 
                 let orderBy = $("#orderBy").val();
-                let setDistinctTags =('#setDistinctTags').val()    
-                if (setDistinctTags === '')
+                let distinctTags = $('#distinctTags').val();
+
+                if (distinctTags != null && distinctTags != -1)
+                    query.append('tag', distinctTags);
+
                 if(orderBy != -1) {
                     let s = orderBy.split('_');
                     query.append('orderBy', s[0]);
                     query.append('orderByType', s[1]);
                 }
-                if(orderBy === 'header_asc') {
-                    query.append('orderBy', 'header');
-                    query.append('orderByType', 'asc');
-                }
-                if(orderBy === 'createdAt_desc') {
-                    query.append('orderBy', 'createdAt');
-                    query.append('orderByType', 'desc');
-                }
-                if(orderBy === 'seen_desc') {
-                    query.append('orderBy', 'seen');
-                    query.append('orderByType', 'desc');
-                }    
                 return query;
             }
             $.ajax({
                 type: 'get',
                 url: '{{ route('api.blog.getDistinctTags') }}',
                 success: function(res) {
-                    console.log(res);
-                    var option = '';
+
+                    var option = '<option value="-1">دسته بندی</option>';
+
                     if(res.status === "ok") {
-                        for(var i = 0; i < res.tags.length; i++) {
-                            option += '<option data-label="' + res.tags[i] + '">' + res.tags[i] + '</option>';
-                        }
-                        $("#setDistinctTags").empty().append(option);
+
+                        for(var i = 0; i < res.tags.length; i++)
+                            option += '<option value="' + res.tags[i] + '">' + res.tags[i] + '</option>';
+
+                        $("#distinctTags").empty().append(option);
                     }
                 }
             });
