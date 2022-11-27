3f027bb346bccea0c8b4c1d81413acf8d3f26359
mghaneh1375 <mghaneh1375@gmail.com>
2022-11-27 15:01:43 +0330

    event session resource addded

--- app/Http/Resources/EventSessionResource.php
+++ app/Http/Resources/EventSessionResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class EventSessionResource extends JsonResource
@@ -14,10 +15,19 @@ class EventSessionResource extends JsonResource
      */
     public function toArray($request)
     {
+
+        $start = date("Y/m/d H:i", $this->start);
+        $end = date("Y/m/d H:i", $this->end);
+
+        $s = Controller::MiladyToShamsi($start, '/');
+        $e = Controller::MiladyToShamsi($end, '/');
+
         return [
             'id' => $this->id,
-            'start' => $this->start,
-            'end' => $this->end,
+            'start_date' => $s,
+            'start_time' => explode(' ', $start)[1],
+            'end_date' => $e,
+            'end_time' => explode(' ', $end)[1],
         ];
     }
 }
